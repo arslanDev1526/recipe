cd57c026f5dd9a70889871343f1c0e8b49065202
Sami Ullah <sami.naazar1526@gmail.com>
2023-08-11 10:39:36 -0700

    show no. of slides as per breakpoints

--- src/pages/home/cooksnap/cookCard.js
+++ src/pages/home/cooksnap/cookCard.js
@@ -41,34 +41,35 @@ function CookCard({ cardsData }) {
     infinite: false,
     nextArrow: <Next />,
     prevArrow: <Prev />,
-
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
     responsive: [
-      {
-        breakpoint: 375,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2,
-        },
-      },
-
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-        
-       
-        },
+          infinite: true,
+          dots: true
+        }
       },
-    ],
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   return (
