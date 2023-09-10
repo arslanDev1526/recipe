f0cf7ecc5b352f9546ab2473b5a8a9a5c30c6e36
unknown <arslanyounas344@gmail.com>
2023-09-10 10:19:43 -0700

    css setting

--- src/pages/profile/tabs/index.js
+++ src/pages/profile/tabs/index.js
@@ -2,19 +2,16 @@ import React, { useState } from "react";
 import { TabsNavbar } from "./TabsNavbar";
 import { TabContent, tabs } from "./tabcontent";
 
-
 export const Tabs = () => {
-    const [activeTab, setActiveTab] = useState(tabs[0]);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
-    const handleTabClick = (tabId) => {
-        setActiveTab(tabId);
-    };
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
 
-    return (
-        <TabsNavbar
-            handleTabClick={handleTabClick}
-            activeTab={activeTab}
-        >
-            <TabContent activeTab={activeTab} />
-        </TabsNavbar>)
-}
+  return (
+    <TabsNavbar handleTabClick={handleTabClick} activeTab={activeTab}>
+      <TabContent activeTab={activeTab} />
+    </TabsNavbar>
+  );
+};
--- src/pages/profile/tabs/tabItems/tips/tips.js
+++ src/pages/profile/tabs/tabItems/tips/tips.js
@@ -46,10 +46,15 @@ export const Tips = () => {
             <div>
               <img src="https://global-web-assets.cpcdn.com/assets/empty_states/no_tips-0c5c91733369f17179a635c3fd99f23cf410e582982af1f2e9459229cd6a26c7.svg" />
             </div>
-            <h2 className={`py-3`}> No tips added yet </h2>
+            <h2 className={`py-3`}> No tips added yet  yes </h2>
           </div>
         </div>
-
+        <Link
+          className={`py-2 px-2 rounded  ${styles["saved-link"]}`}
+          to="./tips"
+        >
+          Create Tips
+        </Link>
       </div>
     </>
   );
